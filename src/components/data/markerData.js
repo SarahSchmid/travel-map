45c60f96bd481edcfb18489f2bc092858a099886
SarahSchmid <sarah.schmid@codecentric.de>
2018-07-03 14:52:13 +0200

    styling test marker

--- src/components/data/markerData.js
+++ src/components/data/markerData.js
@@ -2,20 +2,23 @@ module.exports = [
 
     // Vienna
     {
-        name: "Cafe Mozart",
+        title: "Cafe Mozart",
         location: "Vienna",
+        description:"Das Cafe Mozart hat bei mir immer einen besonderen Platz im Herzen. Es ist eines der traditionsreichen Kaffeehäuser in Wien",
         lat: 48.204332,
         lng: 16.369427
     },
     {
-        name: "Naschmarkt",
+        title: "Naschmarkt",
         location: "Vienna",
+        description:"test test bla bla",
         lat: 48.198649,
         lng: 16.363116
     },
     {
-        name: "",
-        location: "",
+        title: "Test Marker",
+        location: "Irgendwo im Ozean :D",
+        description:"test test bla bla",
         lat: 0,
         lng: 1
     }
