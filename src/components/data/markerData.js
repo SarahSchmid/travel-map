41ffc987bce9586d7d8dce29154191f8a5a19bf3
SarahSchmid <sarah.schmid@codecentric.de>
2018-07-03 16:17:43 +0200

    added markers in vienna

--- src/components/data/markerData.js
+++ src/components/data/markerData.js
@@ -4,22 +4,37 @@ module.exports = [
     {
         title: "Cafe Mozart",
         location: "Vienna",
-        description:"Das Cafe Mozart hat bei mir immer einen besonderen Platz im Herzen. Es ist eines der traditionsreichen Kaffeehäuser in Wien",
+        description:"Cafe Mozart is one of the traditional coffee places, the so called 'Kaffeehäuser' in Vienna. " +
+                    "For me it's one of the most beautiful places in Vienna. The atmosphere takes you back to the 18th " +
+                    "century and if you like cakes and coffee it's definitely the place to be.",
         lat: 48.204332,
         lng: 16.369427
     },
     {
         title: "Naschmarkt",
         location: "Vienna",
-        description:"test test bla bla",
+        description:"You can buy almost everything on the 'Naschmarkt'. Fish, meat, cheese, veggies and also non-food " +
+                    "like soap, clothes and many more, but you definitely need a big wallet. The booths are looking very " +
+                    "charming and the sellers know how to sell their stuff.",
         lat: 48.198649,
         lng: 16.363116
     },
     {
-        title: "Test Marker",
-        location: "Irgendwo im Ozean :D",
-        description:"test test bla bla",
-        lat: 0,
-        lng: 1
+        title: "Schönbrunn",
+        location: "Vienna",
+        description:"The Schönbrunn Palace was the home of the legendary empress Sissi and it still has it's charms. " +
+                    "The bright and colorful garden, the zoo and you can overview the whole city from the Gloriette. " +
+                    "When you're visiting in december, be sure to visit the traditional christmas market. Mulled wine, " +
+                    "small souvenirs and delicious snacks are waiting.",
+        lat: 48.186048,
+        lng: 16.312743
+    },
+    {
+        title: "Prater",
+        location: "Vienna",
+        description:"A funfair that opens the whole year? Such a think doesn't exist? It does! It's called 'Wiener Prater' " +
+                    "and it's located in the district Leopoldstadt. The ferriswheel is legendary and the view is incredible!",
+        lat: 48.216941,
+        lng: 16.398061
     }
 ];
